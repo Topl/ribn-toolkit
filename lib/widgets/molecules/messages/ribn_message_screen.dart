4edac96d647e3cba4584cb093e3d37cbe370809b
Siphamandla Mjoli <g14m1190@campus.ru.ac.za>
2023-02-10 17:58:43 +0200

    fix(message_screen): fixed yellow underline

--- lib/widgets/molecules/messages/ribn_message_screen.dart
+++ lib/widgets/molecules/messages/ribn_message_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import '../../../constants/assets.dart';
 import '../../../constants/colors.dart';
+import '../../atoms/custom_icon_button.dart';
 import '../../atoms/large_button.dart';
 import '../../atoms/text/ribn_font13_text_widget.dart';
 import '../../atoms/text/ribn_h3_text_widget.dart';
@@ -33,77 +34,99 @@ class RibnMessageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-        body: WaveContainer(
-      containerChild: Padding(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 50),
-                child: RibnH3TextWidget(
-                  text: title,
-                  textAlign: TextAlign.justify,
-                  textColor: RibnColors.whiteColor,
-                  fontWeight: FontWeight.w700,
+    return WaveContainer(
+      containerChild: Material(
+        type: MaterialType.transparency,
+        child: Padding(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 50, left: 100),
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: CustomIconButton(
+                      icon: const Icon(
+                        Icons.close,
+                        color: RibnColors.lightGreyTitle,
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      highlightColor: Colors.transparent,
+                      hoverColor: Colors.transparent,
+                      splashColor: Colors.transparent,
+                    ),
+                  ),
                 ),
               ),
-            ),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: Image.asset(
-                  isError ? RibnAssets.errorFace : RibnAssets.checkCircleIcon,
-                  scale: isError ? 2 : 0.5,
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 50),
+                  child: RibnH3TextWidget(
+                    text: title,
+                    textAlign: TextAlign.justify,
+                    textColor: RibnColors.whiteColor,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
               ),
-            ),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: RibnFont13TextWidget(
-                  text: topMessage,
-                  textAlign: TextAlign.justify,
-                  textColor: RibnColors.whiteColor,
-                  fontWeight: FontWeight.w400,
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Image.asset(
+                    isError ? RibnAssets.errorFace : RibnAssets.checkCircleIcon,
+                    scale: isError ? 2 : 0.5,
+                  ),
                 ),
               ),
-            ),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: RibnFont13TextWidget(
-                  text: bottomMessage,
-                  textAlign: TextAlign.justify,
-                  textColor: RibnColors.whiteColor,
-                  fontWeight: FontWeight.w400,
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: RibnFont13TextWidget(
+                    text: topMessage,
+                    textAlign: TextAlign.justify,
+                    textColor: RibnColors.whiteColor,
+                    fontWeight: FontWeight.w400,
+                  ),
                 ),
               ),
-            ),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 80),
-                child: LargeButton(
-                  buttonChild: RibnFont13TextWidget(
-                      text: buttonTitle,
-                      textAlign: TextAlign.center,
-                      textColor: buttonTitleColor,
-                      fontWeight: FontWeight.normal),
-                  backgroundColor: RibnColors.primaryOffColor,
-                  hoverColor: RibnColors.primaryButtonHover,
-                  dropShadowColor: RibnColors.primaryButtonShadow,
-                  onPressed: onTap,
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: RibnFont13TextWidget(
+                    text: bottomMessage,
+                    textAlign: TextAlign.justify,
+                    textColor: RibnColors.whiteColor,
+                    fontWeight: FontWeight.w400,
+                  ),
                 ),
               ),
-            )
-          ],
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 80),
+                  child: LargeButton(
+                    buttonChild: RibnFont13TextWidget(
+                        text: buttonTitle,
+                        textAlign: TextAlign.center,
+                        textColor: buttonTitleColor,
+                        fontWeight: FontWeight.normal),
+                    backgroundColor: RibnColors.primaryOffColor,
+                    hoverColor: RibnColors.primaryButtonHover,
+                    dropShadowColor: RibnColors.primaryButtonShadow,
+                    onPressed: onTap,
+                  ),
+                ),
+              )
+            ],
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
         ),
-        padding: const EdgeInsets.symmetric(horizontal: 20),
       ),
       containerWidth: width,
       containerHeight: height,
-    ));
+    );
   }
 }
