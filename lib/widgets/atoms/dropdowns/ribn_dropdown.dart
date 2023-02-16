c0d7b7e864e7762a9894690e2fba70ff36530569
Siphamandla Mjoli <g14m1190@campus.ru.ac.za>
2023-02-16 03:52:38 +0200

    fix(ribn_drop_down): fixed centering issue

--- lib/widgets/atoms/dropdowns/ribn_dropdown.dart
+++ lib/widgets/atoms/dropdowns/ribn_dropdown.dart
@@ -53,163 +53,156 @@ class RibnDropdown extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return SizedBox(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 8),
-            child: Row(
-              children: [
-                RibnBodyFont12TextWidget(
-                  text: title,
-                  textAlign: TextAlign.justify,
-                  textColor: titleColor,
-                  fontWeight: titleWeight,
-                )
-              ],
-            ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(bottom: 8),
+          child: Row(
+            children: [
+              RibnBodyFont12TextWidget(
+                text: title,
+                textAlign: TextAlign.justify,
+                textColor: titleColor,
+                fontWeight: titleWeight,
+              )
+            ],
           ),
-          Container(
-              height: 40,
-              width: 350,
-              decoration: BoxDecoration(
-                  color:
-                      Colors.transparent, //background color of dropdown button
-                  border: Border.all(
-                      color: RibnColors.greyText,
-                      width: 0.3), //border of dropdown button
-                  borderRadius: BorderRadius.circular(
-                      5), //border raiuds of dropdown button
-                  boxShadow: const <BoxShadow>[
-                    //apply shadow on Dropdown button
-                    BoxShadow(color: Colors.transparent) //blur radius of shadow
-                  ]),
-              child: Padding(
-                  padding: const EdgeInsets.only(left: 5, right: 5),
-                  child: Row(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                            color: Colors.grey[
-                                300], //background color of dropdown button
-                            border: Border.all(
-                                color: Colors.grey.withOpacity(1),
-                                width: 0.011), //border of dropdown button
-                            borderRadius: BorderRadius.circular(
-                                3), //border raiuds of dropdown button
-                            boxShadow: const <BoxShadow>[
-                              //apply shadow on Dropdown button
-                              BoxShadow(
-                                  color: Colors
-                                      .transparent) //blur radius of shadow
-                            ]),
-                        width: 286,
-                        height: 30,
-                        child: Padding(
-                          padding: const EdgeInsets.only(right: 2),
-                          child: DropdownButton(
-                            underline: Container(),
-                            hint: Padding(
-                              child: Text(dropDownHintText,
-                                  style: const TextStyle(
-                                      color: RibnColors.defaultText)),
-                              padding: const EdgeInsets.only(top: 10, left: 10),
-                            ),
-                            key: _dropdownButtonKey,
-                            value: dropDownValue,
-                            selectedItemBuilder: (BuildContext context) =>
-                                dropDownItems
-                                    .map<DropdownMenuItem<String>>(
-                                        (String item) =>
-                                            DropdownMenuItem<String>(
-                                              value: item,
-                                              child: Container(
-                                                alignment: Alignment.centerLeft,
-                                                constraints:
-                                                    const BoxConstraints(
-                                                        minHeight: 48.0),
-                                                child: Padding(
-                                                  child: Text(
-                                                    item,
-                                                    style: const TextStyle(
-                                                        color: RibnColors
-                                                            .defaultText),
-                                                  ),
-                                                  padding:
-                                                      const EdgeInsets.only(
-                                                          left: 5),
-                                                ),
-                                              ),
-                                            ))
-                                    .toList(),
-                            borderRadius: BorderRadius.circular(11.4),
-                            items: dropDownItems
-                                .map<DropdownMenuItem<String>>((String item) =>
-                                    DropdownMenuItem<String>(
-                                      value: item,
-                                      child: Container(
-                                        alignment: Alignment.centerLeft,
-                                        constraints: const BoxConstraints(
-                                            minHeight: 48.0),
-                                        child: Text(
-                                          item,
-                                          style: const TextStyle(
-                                              color: RibnColors.defaultText),
-                                        ),
-                                      ),
-                                    ))
-                                .toList(),
-                            onChanged: onChanged,
-                            icon: Container(),
-                            iconEnabledColor: Colors.white, //Icon color
-                            style: const TextStyle(
-                                //te
-                                color: RibnColors.greyText, //Font color
-                                fontSize: 12 //font size on dropdown button
-                                ),
-
-                            dropdownColor:
-                                Colors.white, //dropdown background color
-                            //underline: Container(), //remove underline
-                            isExpanded: true, //make true to make width 100%
+        ),
+        Container(
+            height: 40,
+            width: 350,
+            decoration: BoxDecoration(
+                color: Colors.transparent, //background color of dropdown button
+                border: Border.all(
+                    color: RibnColors.greyText,
+                    width: 0.3), //border of dropdown button
+                borderRadius:
+                    BorderRadius.circular(5), //border raiuds of dropdown button
+                boxShadow: const <BoxShadow>[
+                  //apply shadow on Dropdown button
+                  BoxShadow(color: Colors.transparent) //blur radius of shadow
+                ]),
+            child: Padding(
+                padding: const EdgeInsets.only(left: 5, right: 5),
+                child: Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors
+                              .grey[300], //background color of dropdown button
+                          border: Border.all(
+                              color: Colors.grey.withOpacity(1),
+                              width: 0.011), //border of dropdown button
+                          borderRadius: BorderRadius.circular(
+                              3), //border raiuds of dropdown button
+                          boxShadow: const <BoxShadow>[
+                            //apply shadow on Dropdown button
+                            BoxShadow(
+                                color:
+                                    Colors.transparent) //blur radius of shadow
+                          ]),
+                      width: 286,
+                      height: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 2),
+                        child: DropdownButton(
+                          underline: Container(),
+                          hint: Padding(
+                            child: Text(dropDownHintText,
+                                style: const TextStyle(
+                                    color: RibnColors.defaultText)),
+                            padding: const EdgeInsets.only(top: 10, left: 10),
                           ),
+                          key: _dropdownButtonKey,
+                          value: dropDownValue,
+                          selectedItemBuilder: (BuildContext context) =>
+                              dropDownItems
+                                  .map<DropdownMenuItem<String>>(
+                                      (String item) => DropdownMenuItem<String>(
+                                            value: item,
+                                            child: Container(
+                                              alignment: Alignment.centerLeft,
+                                              constraints: const BoxConstraints(
+                                                  minHeight: 48.0),
+                                              child: Padding(
+                                                child: Text(
+                                                  item,
+                                                  style: const TextStyle(
+                                                      color: RibnColors
+                                                          .defaultText),
+                                                ),
+                                                padding: const EdgeInsets.only(
+                                                    left: 5),
+                                              ),
+                                            ),
+                                          ))
+                                  .toList(),
+                          borderRadius: BorderRadius.circular(11.4),
+                          items: dropDownItems
+                              .map<DropdownMenuItem<String>>((String item) =>
+                                  DropdownMenuItem<String>(
+                                    value: item,
+                                    child: Container(
+                                      alignment: Alignment.centerLeft,
+                                      constraints:
+                                          const BoxConstraints(minHeight: 48.0),
+                                      child: Text(
+                                        item,
+                                        style: const TextStyle(
+                                            color: RibnColors.defaultText),
+                                      ),
+                                    ),
+                                  ))
+                              .toList(),
+                          onChanged: onChanged,
+                          icon: Container(),
+                          iconEnabledColor: Colors.white, //Icon color
+                          style: const TextStyle(
+                              //te
+                              color: RibnColors.greyText, //Font color
+                              fontSize: 12 //font size on dropdown button
+                              ),
+
+                          dropdownColor:
+                              Colors.white, //dropdown background color
+                          //underline: Container(), //remove underline
+                          isExpanded: true, //make true to make width 100%
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10),
-                        child: InkWell(
-                          child: Container(
-                            decoration: BoxDecoration(
-                                color: Colors.grey[
-                                    300], //background color of dropdown button
-                                border: Border.all(
-                                    color: Colors.grey.withOpacity(1),
-                                    width: 0.011), //border of dropdown button
-                                borderRadius: BorderRadius.circular(
-                                    3), //border raiuds of dropdown button
-                                boxShadow: const <BoxShadow>[
-                                  //apply shadow on Dropdown button
-                                  BoxShadow(
-                                      color: Colors
-                                          .transparent) //blur radius of shadow
-                                ]),
-                            width: 40,
-                            height: 30,
-                            child: const Icon(
-                              Icons.arrow_drop_down_sharp,
-                              color: RibnColors.defaultText,
-                            ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: InkWell(
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.grey[
+                                  300], //background color of dropdown button
+                              border: Border.all(
+                                  color: Colors.grey.withOpacity(1),
+                                  width: 0.011), //border of dropdown button
+                              borderRadius: BorderRadius.circular(
+                                  3), //border raiuds of dropdown button
+                              boxShadow: const <BoxShadow>[
+                                //apply shadow on Dropdown button
+                                BoxShadow(
+                                    color: Colors
+                                        .transparent) //blur radius of shadow
+                              ]),
+                          width: 40,
+                          height: 30,
+                          child: const Icon(
+                            Icons.arrow_drop_down_sharp,
+                            color: RibnColors.defaultText,
                           ),
-                          onTap: openDropdown,
                         ),
-                      )
-                    ],
-                  ))),
-        ],
-      ),
-      width: MediaQuery.of(context).size.width,
+                        onTap: openDropdown,
+                      ),
+                    )
+                  ],
+                ))),
+      ],
     );
   }
 }
